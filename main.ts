78133137b5af519d6014fb3c68fe834dca4f3441
craftpage <craftpage@gmail.com>
2023-08-11 11:05:10 +0900

    MakeCodeプロジェクトの初期ファイル

---
+++ main.ts
@@ -0,0 +1,7 @@
+basic.forever(function () {
+    if (input.lightLevel() > 3) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+    } else {
+        pins.digitalWritePin(DigitalPin.P2, 0)
+    }
+})
